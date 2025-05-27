c96833f29da9e1f78024b7f9b940cd0c0f6b5162
sky <blooming_garden_@naver.com>
2025-05-27 16:45:22 +0900

    Fix: 제발...

--- src/app/my-gallery/page.jsx
+++ src/app/my-gallery/page.jsx
@@ -44,7 +44,6 @@ export default function MyPage() {
             genreCounts={genreForFilter}
           /> */}
         </div>
-        {console.log(filteredData)}
         {/* 카드 렌더링 ↓ */}
         <div className="grid grid-cols-2 lg:grid-cols-3">
           {/* {filteredData.length > 0 && */}
