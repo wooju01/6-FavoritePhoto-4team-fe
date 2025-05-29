62c4794a4233729516ace446355299db104338ec
LEE JI SU <afafmm1903@gmail.com>
2025-05-29 16:46:36 +0900

    로그인 모달 최종 수정

--- src/components/ui/BaseCard.jsx
+++ src/components/ui/BaseCard.jsx
@@ -5,6 +5,7 @@ import favicon from "@/assets/favicon.svg";
 import GradeTag from "../tag/GradeTag";
 import example from "@/assets/example.svg";
 import Link from "next/link";
+import NotiClickHandler from "@/components/HompageComponents/NotiClickHandler";
 
 const genreMap = {
   1: "여행",
@@ -18,63 +19,69 @@ export default function BaseCard({
   photoCard,
   price,
   saleQuantity,
-  seller
+  seller,
 }) {
   return (
-    <Link href={`/home/${id}`}>
-      <div className="flex flex-col gap-2.5 md:gap-5 p-[15px] md:p-5 lg:p-10 border-1 border-white/10 rounded-xs bg-gray-500">
-        <img src={`https://six-favoritephoto-4team-be.onrender.com${photoCard.imageUrl}`} alt="photo-img" className="aspect-square" />
-        <div className="flex flex-col gap-2.5 md:gap-5">
-          <div>
-            <h2 className="text-700-14 md:text-700-22 mb-1.5 md:mb-2.5">
-              {photoCard.name}
-            </h2>
-            <div className="flex items-center justify-between gap-1 md:gap-0">
-              <div className="flex items-center justify-between">
-                <div className="flex [&_*]:flex ">
-                  <div className="items-center justify-center gap-1 md:gap-2.5">
-                    <p className="[&_*]:text-300-10 md:[&_*]:text-300-16">
-                      <GradeTag grade={photoCard.gradeId} />
-                    </p>
-                    <span className="h-3 md:h-5 w-[1px] bg-gray-400 md:"></span>
-                    <span className="text-nowrap text-400-10 md:text-400-16 text-gray-300">
-                      {genreMap[photoCard.genreId]}
-                    </span>
+    <NotiClickHandler>
+      <Link href={`/home/${id}`}>
+        <div className="flex flex-col gap-2.5 md:gap-5 p-[15px] md:p-5 lg:p-10 border-1 border-white/10 rounded-xs bg-gray-500">
+          <img
+            src={`https://six-favoritephoto-4team-be.onrender.com${photoCard.imageUrl}`}
+            alt="photo-img"
+            className="aspect-square"
+          />
+          <div className="flex flex-col gap-2.5 md:gap-5">
+            <div>
+              <h2 className="text-700-14 md:text-700-22 mb-1.5 md:mb-2.5">
+                {photoCard.name}
+              </h2>
+              <div className="flex items-center justify-between gap-1 md:gap-0">
+                <div className="flex items-center justify-between">
+                  <div className="flex [&_*]:flex ">
+                    <div className="items-center justify-center gap-1 md:gap-2.5">
+                      <p className="[&_*]:text-300-10 md:[&_*]:text-300-16">
+                        <GradeTag grade={photoCard.gradeId} />
+                      </p>
+                      <span className="h-3 md:h-5 w-[1px] bg-gray-400 md:"></span>
+                      <span className="text-nowrap text-400-10 md:text-400-16 text-gray-300">
+                        {genreMap[photoCard.genreId]}
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div>
-                {/* 닉네임 */}
-                <span className="text-400-10 md:text-400-16 underline line-clamp-1">
-                  {seller.nickname}
-                </span>
+                <div>
+                  {/* 닉네임 */}
+                  <span className="text-400-10 md:text-400-16 underline line-clamp-1">
+                    {seller.nickname}
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="w-full h-[1px] bg-gray-400">{/* LINE */}</div>
-          <div
-            className="
+            <div className="w-full h-[1px] bg-gray-400">{/* LINE */}</div>
+            <div
+              className="
           flex flex-col gap-[5px] md:gap-2.5 [&>div]:flex [&>div]:justify-between
             [&_p]:text-300-10 md:[&_p]:text-300-16 [&_p]:text-gray-300
             [&_span]:text-400-10 md:[&_span]:text-400-16 
           "
-          >
-            <div>
-              <p>가격</p>
-              <span>{price} P</span>
-            </div>
-            <div>
-              <p>잔여</p>
-              <span>
-                {saleQuantity} / <span className="text-gray-300">10</span>
-              </span>
+            >
+              <div>
+                <p>가격</p>
+                <span>{price} P</span>
+              </div>
+              <div>
+                <p>잔여</p>
+                <span>
+                  {saleQuantity} / <span className="text-gray-300">10</span>
+                </span>
+              </div>
             </div>
           </div>
+          <div className="hidden md:flex justify-center">
+            <Image src={favicon} alt="logo" className="w-24 h-4" />
+          </div>
         </div>
-        <div className="hidden md:flex justify-center">
-          <Image src={favicon} alt="logo" className="w-24 h-4" />
-        </div>
-      </div>
-    </Link>
+      </Link>
+    </NotiClickHandler>
   );
 }
--- src/components/ui/SellButton.jsx
+++ src/components/ui/SellButton.jsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import MyCardModal from "../CardSeller/MyCardModal";
+import NotiClickHandler from "@/components/HompageComponents/NotiClickHandler";
 
 export default function SellButton() {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,12 +13,14 @@ export default function SellButton() {
   return (
     <>
       <div className="fixed w-full md:max-w-[342px] lg:max-w-[440px] px-4 bottom-4 left-0 z-40 md:static md:px-0">
-        <button
-          onClick={handleClick}
-          className="flex justify-center items-center w-full h-14 md:h-[60px] rounded-xs cursor-pointer text-700-16 text-my-black bg-main"
-        >
-          나의 포토카드 판매하기
-        </button>
+        <NotiClickHandler>
+          <button
+            onClick={handleClick}
+            className="flex justify-center items-center w-full h-14 md:h-[60px] rounded-xs cursor-pointer text-700-16 text-my-black bg-main"
+          >
+            나의 포토카드 판매하기
+          </button>
+        </NotiClickHandler>
       </div>
 
       <MyCardModal isOpen={isOpen} onClose={handleClose} />
