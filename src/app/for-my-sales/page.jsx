11f89e3fad752cb889f39a65628072598256f3ed
sky <blooming_garden_@naver.com>
2025-05-29 15:10:33 +0900

    chore: build 오류 나서 잠깐 주석 처리

--- src/app/for-my-sales/page.jsx
+++ src/app/for-my-sales/page.jsx
@@ -10,10 +10,10 @@ import Search from "@/components/ui/Search";
 import example from "@/assets/example.svg";
 
 export default function ForSalePage() {
-  const { data, isPending, isError } = useQuery({
-    queryKey: ["mysales"],
-    queryFn: getMyCardsOnSale,
-  });
+  // const { data, isPending, isError } = useQuery({
+  //   queryKey: ["mysales"],
+  //   queryFn: getMyCardsOnSale,
+  // });
 
   // 카드 개수 불러오기 (전체 + 등급별)
   const { data: count } = useQuery({
@@ -38,14 +38,14 @@ export default function ForSalePage() {
       <header className="hidden md:block my-10">
         <Title title="나의 판매 포토카드" font="titleLg_Bk" />
       </header>
-      <OwnedCards
+      {/* <OwnedCards
         name={data?.owner?.nickname}
         totalCards={count?.inactive.total}
         countsByGrade={count?.inactive.byGrade}
-      />
+      /> */}
       <section className="mb-15">
         <div className="flex items-center mb-5">
-          <Search />
+          {/* <Search /> */}
           {/*<FilterDropdown
             visibleFilters={["grade", "genre"]}
             gradeCounts={gradeForFilter}
