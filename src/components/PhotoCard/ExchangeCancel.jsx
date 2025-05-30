6519b5a923938eaffb63794c580732bbd4267414
Wooju <dnwn810@naver.com>
2025-05-30 10:50:43 +0900

    feat: 리베이스 하고 교환요청 하기 추가

--- src/components/PhotoCard/ExchangeCancel.jsx
+++ src/components/PhotoCard/ExchangeCancel.jsx
@@ -30,7 +30,7 @@ export default function ExchangeCancel({
       {/* 이미지 */}
       <div className="w-full aspect-[4/3] relative mb-3">
         <img
-          src={{image}}
+          src={image}
           alt={name}
           className="object-cover"
         />
