a1e35e367bb209fbc8c37d4230c83dc3af701323
Wooju <dnwn810@naver.com>
2025-06-02 14:28:26 +0900

    리베이스

--- src/hooks/useFilterSheet.js
+++ src/hooks/useFilterSheet.js
@@ -36,32 +36,37 @@ export function useFilterSheet(fetcher) {
     }
   };
 
-  const fetchFilteredCount = async (rawFilters) => {
+const fetchFilteredCount = async (rawFilters) => {
   setLoading(true);
   try {
-    const filters = typeof rawFilters.grade === "string"
-      ? parseFilters(rawFilters)
-      : rawFilters;
+    // rawFilters 는 { grade: "1,2", ... } 또는 { grade: [1,2], ... } 형태가 올 수 있음
+    // 만약 문자열이면 배열로 바꾸고, 배열이면 그대로 사용
+    const filters = {
+      grade: typeof rawFilters.grade === "string"
+        ? rawFilters.grade.split(",").map(Number)
+        : rawFilters.grade || [],
+      genre: typeof rawFilters.genre === "string"
+        ? rawFilters.genre.split(",").map(Number)
+        : rawFilters.genre || [],
+      sale: typeof rawFilters.sale === "string"
+        ? rawFilters.sale.split(",")
+        : rawFilters.sale || [],
+      saleType: typeof rawFilters.saleType === "string"
+        ? rawFilters.saleType.split(",")
+        : rawFilters.saleType || [],
+    };
 
     const query = {};
-    if (Array.isArray(filters.grade) && filters.grade.length) {
-      query.grade = filters.grade.join(",");
-    }
-    if (Array.isArray(filters.genre) && filters.genre.length) {
-      query.genre = filters.genre.join(",");
-    }
-    if (Array.isArray(filters.sale) && filters.sale.length) {
-      query.sale = filters.sale.join(",");
-    }
-    if (Array.isArray(filters.saleType) && filters.saleType.length) {
-      query.saleType = filters.saleType.join(",");
-    }
+    if (filters.grade.length) query.grade = filters.grade.join(",");
+    if (filters.genre.length) query.genre = filters.genre.join(",");
+    if (filters.sale.length) query.sale = filters.sale.join(",");
+    if (filters.saleType.length) query.saleType = filters.saleType.join(",");
 
     console.log("query sent to fetcher:", query);
     const data = await fetcher(query, true);
     console.log("response from fetcher:", data);
 
-
+    // filteredCount 설정 로직
     if (Array.isArray(data.sales)) {
       setFilteredCount(data.sales.length);
     } else if (data.pagination?.totalItems !== undefined) {
@@ -78,6 +83,7 @@ export function useFilterSheet(fetcher) {
     setLoading(false);
   }
 };
+
 
 
   return {
