efb66374fa387d7a40e72aa2f9e906ba10f656e5
Wooju <dnwn810@naver.com>
2025-06-03 14:28:05 +0900

    seo 재배포 home에 페이지에 있음

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -4,6 +4,17 @@ import RandomPoint from "@/components/RandomPoint";
 import RandomPointHomeTrigger from "@/components/RandomPointHomeTrigger";
 import NotiModal from "@/components/modal/NotiModal";
 
+export const metadata = {
+  title: "최악의 포토",
+  description: "포토카드를 확인하세요",
+  openGraph: {
+    title: "최애의 포토",
+    description: "사진을 만들고 공유할 수 있는 커뮤니티입니다.",
+    type: "website",
+    images: ["https://6-favorite-photo-4team-fe.vercel.app/contents.png"],
+  },
+};
+
 export default async function HomePage({ searchParams }) {
   const params = await searchParams;
 
